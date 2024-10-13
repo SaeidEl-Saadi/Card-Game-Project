98449d62b772fe84fda2044dd9a4e64b886eed57
Saeid El-Saadi <121737118+SaeidEl-Saadi@users.noreply.github.com>
2024-10-12 20:38:13 -0400

    R-TEST: RESP-06 general outputs

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.io.PrintWriter;
@@ -209,6 +210,89 @@ public class MainTest {
         for (int i = 0; i < 4; i++) {
             assertEquals(2, game.getPlayers().get(i).getCards().size());
         }
+    }
+
+    @Test
+    @DisplayName("Order hand")
+    void RESP_6_test_1() {
+        Game game = new Game();
+
+        Weapon first = new Weapon("H10", 10);
+        Weapon second = new Weapon("D5", 5);
+        Weapon third = new Weapon("S10", 10);
+        Foe fourth = new Foe("F15", 15);
+        Foe fifth = new Foe("F50", 50);
+
+        game.getCurrentPlayer().addCard(first);
+        game.getCurrentPlayer().addCard(second);
+        game.getCurrentPlayer().addCard(third);
+        game.getCurrentPlayer().addCard(fourth);
+        game.getCurrentPlayer().addCard(fifth);
+
+        assertEquals("F15", game.getCurrentPlayer().getCards().get(0).getCard());
+        assertEquals("F50", game.getCurrentPlayer().getCards().get(1).getCard());
+        assertEquals("D5", game.getCurrentPlayer().getCards().get(2).getCard());
+        assertEquals("S10", game.getCurrentPlayer().getCards().get(3).getCard());
+        assertEquals("H10", game.getCurrentPlayer().getCards().get(4).getCard());
+    }
+
+    @Test
+    @DisplayName("Display player's hand")
+    void RESP_6_test_2() {
+        Game game = new Game();
+        UI ui = new UI();
+        game.dealCards();
+
+        PrintStream previous = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        ui.displayHand(game.getCurrentPlayer());
+        String output = outputStream.toString().trim();
+
+        String expected = game.getCurrentPlayer().getName() + "'s hand:";
+        for (int i = 0; i < 12; i++) {
+            expected += "\n" + (i + 1) + ". " + game.getCurrentPlayer().getCards().get(i);
+        }
+
+        System.setOut(previous);
+        assertEquals(expected.trim(), output);
+    }
+
+    @Test
+    @DisplayName("Players swapping turns")
+    void RESP_6_test_3() {
+        Game game = new Game();
+        UI ui = new UI();
+        game.dealCards();
+
+        String input = "\n";
+        ByteArrayInputStream userInput = new ByteArrayInputStream(input.getBytes());
+        System.setIn(userInput);
+
+        PrintStream previous = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        ui.hotSeatPrompt(game);
+        Player p = game.getCurrentPlayer();
+        game.nextTurn();
+        ui.displayHand(game.getCurrentPlayer());
+        String output = outputStream.toString();
+
+        String expected = "";
+        for (int i = 0; i < 20; i++) {
+            expected += "\n";
+        }
+        expected += p.getName() + "'s turn has ended. Press enter for next turn...\n";
+        expected += game.getCurrentPlayer().getName() + "'s hand:";
+        for (int i = 0; i < 12; i++) {
+            expected += "\n" + (i + 1) + ". " + game.getCurrentPlayer().getCards().get(i);
+        }
+        expected += "\n";
+
+        System.setOut(previous);
+        assertEquals(expected, output);
     }
 
 }
